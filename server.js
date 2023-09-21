5418fc4c73b88353d04d1a3d3e1e3f7555e50169
Nicole Torres <nicolejoan9@yahoo.com>
2023-09-21 13:14:12 -0400

    fixed pathing to see if server is running

--- server.js
+++ server.js
@@ -2,29 +2,44 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const notes = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/api/notes", (req, res) => res.sendFile(path.join(_dirname, "/db/db.json")));
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/db/db.json"))
+);
 
 app.post("/api/notes", (req, res) => {
-const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-const newNotes = req.body;
-notes.push(newNotes);
-fs.writeFileSync("./db/db.json", JSON.stringify(notes))
-res.json(notes);
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const newNotes = req.body;
+  newNotes.id = uuidv4();
+  notes.push(newNotes);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  res.json(notes);
+  console.log(notes);
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/assets/notes.html"));
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.get("*", (req, res) => {
-  return res.sendFile(path.join(__dirname, "public/assets/index.html"));
+  return res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.listen(PORT);
+app.delete("/api/notes/:id", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const nuevaNote = notes.filter((remove) => remove.id !== req.params.id);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(nuevaNote));
+  res.json("Deleted");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
